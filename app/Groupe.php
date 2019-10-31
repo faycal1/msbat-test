8784420a80ac2e25311d0c72b27d513006606a16
vagrant <vagrant@homestead>
2019-10-31 14:24:53 +0000

    ...

--- app/Groupe.php
+++ app/Groupe.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Groupe extends Model
 {
+    protected $fillable = ['name'];
+
     public function groupes()
     {
         return $this->belongsToMany(User::class);
